dd4a780db7b702afa14b2ecbf6db672989feaf13
Fazsu <miska.romppainen@hotmail.com>
2022-08-25 06:49:38 +0300

    Updated MATLAB GUI
    +Added Claw functions
    +Altered the overall GUI to reserve space for Claw buttons

--- Matlab/IKmearm/FKdraw.m
+++ Matlab/IKmearm/FKdraw.m
@@ -2,8 +2,24 @@ function [ FK ] = FKdraw( j1,j2,j3 )
 %initial parameter
 %[j0 j1 j2 j3;d0 d1 d2 d3;a0 a1 a2 a3;t0 t1 t2 t3]
 %theta3=360-theta1-theta2
-j4=360-(j2+j3);
-j=[j1 j2 j3 j4;2 0 0 0;0 10 10 2;90 0 0 0];
+
+%Original j4: 360-(j2+j3)
+% Original j: 
+%   [j1 j2 j3 j4;
+%   2 0 0 0;
+%   0 10 10 2;
+%   90 0 0 0];
+
+%j=(2,1): alustan korkeus (base height)
+%j=(3,2): kulma1 - kulma2 etäisyys (angle1 - angle2 length)
+%j=(3,3): kulma2 - kulma3 etäisyys (angle2 - angle3 length)
+%j=(3,4): kulma3 - päätetyöväline etäisyys (angle3-claw length)
+
+j4= 330-(j2+j3);     
+j=[j1 j2 j3 j4; 
+    5 0 0 0; 
+    0 14 14 4;
+    90 0 0 0];
 FK=DHkine(j);
 Q=XYZkine(FK);
 %J=Jacobian(FK); 
@@ -13,7 +29,7 @@ title('3DOF Pseudo Inverse Jacobian Inverse Kinematic')
 xlabel('X Axis');
 ylabel('Y Axis');
 zlabel('Z Axis');
-axis([-20 20 0 20 0 20]);
+axis([-30 30 0 30 0 30]);
 h = rotate3d;
 h.Enable = 'on';
 h.ActionPostCallback = @mypostcallback;
